0382b90688e1e11a689b87a151e1aa87392965c3
Adnriz <146065555+Adnriz@users.noreply.github.com>
2023-11-29 09:53:45 +0100

    Update MainControllor.java

--- src/MainControllor.java
+++ src/MainControllor.java
@@ -1,14 +1,6 @@
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
 
 public class MainControllor {
-    public void songPauseButton(MouseEvent mouseEvent) {
-    }
-
-    public void songSkipButton(MouseEvent mouseEvent) {
-    }
-
-    public void songBackButton(MouseEvent mouseEvent) {
-    }
 
     public void playMedia() {
 
