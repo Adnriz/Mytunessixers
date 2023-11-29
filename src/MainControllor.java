72fb6c94b7e703e7b964cc9a8faade0d1594000a
Adnriz <146065555+Adnriz@users.noreply.github.com>
2023-11-29 09:41:23 +0100

    new

--- src/MainControllor.java
+++ src/MainControllor.java
@@ -9,4 +9,20 @@ public class MainControllor {
 
     public void songBackButton(MouseEvent mouseEvent) {
     }
+
+    public void playMedia() {
+
+    }
+
+    public void pauseMedia() {
+
+    }
+
+    public void previousMedia() {
+
+    }
+
+    public void nextMedia() {
+
+    }
 }
