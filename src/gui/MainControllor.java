ff948f0ed265f3ba15748b47a39a62a7d70d407a
Autz3n <madsautzen@gmail.com>
2023-11-29 11:48:23 +0100

    ffs

--- src/gui/MainControllor.java
+++ src/gui/MainControllor.java
@@ -1,6 +1,15 @@
 package gui;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainControllor {
 
@@ -18,5 +27,15 @@ public class MainControllor {
 
     public void nextMedia() {
 
+    }
+@FXML
+    private void addNewSong(ActionEvent actionEvent) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SongManager.fxml"));
+        Parent root = loader.load();
+        SongControllor songControllor = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Song Manager");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
