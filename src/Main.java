8dbd48043d28a35383488ebbd782e3ddb0f688cf
Adnriz <146065555+Adnriz@users.noreply.github.com>
2023-11-29 09:27:13 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        Parent rootScene = loader.load();
+        primaryStage.setScene(new Scene(rootScene));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("MyTunes - The number 1 music player for windows");
+    }
+}
---
+++ src/MainControllor.java
@@ -0,0 +1,12 @@
+import javafx.scene.input.MouseEvent;
+
+public class MainControllor {
+    public void songPauseButton(MouseEvent mouseEvent) {
+    }
+
+    public void songSkipButton(MouseEvent mouseEvent) {
+    }
+
+    public void songBackButton(MouseEvent mouseEvent) {
+    }
+}
