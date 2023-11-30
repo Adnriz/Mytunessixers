25c67465d02a4d1e9ea4823f8dbbf6dd643a0369
Autz3n <madsautzen@gmail.com>
2023-11-30 10:19:28 +0100

    Man kan ny gemme filer

--- src/gui/SongControllor.java
+++ src/gui/SongControllor.java
@@ -3,12 +3,17 @@ package gui;
 import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.io.File;
-import java.security.cert.Extension;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 
 public class SongControllor {
+
+    private static final String MUSIC_SAVER = ".idea/Music";
 
 
     public void fileChoose(ActionEvent actionEvent) {
@@ -21,6 +26,8 @@ public class SongControllor {
             String modifiedFileName = selectedFile.getName().toLowerCase();
             if (modifiedFileName.endsWith(".mp3") || modifiedFileName.endsWith(".wav")) {
                 System.out.println("Virker");
+                saveSong(selectedFile);
+
             }
             else {
                     System.out.println("forkert fil type");
@@ -29,7 +36,24 @@ public class SongControllor {
         else {
             System.out.println("Virker ikke");
         }
+    }
+    private void saveSong(File selectedFile) {
+        //Gemmer den mappe som filen skal ligges i
+        File saveFolder = new File(MUSIC_SAVER);
+        //tjekker om mappen findes
+        if (!saveFolder.exists()) {
+            System.out.println("Mappen findes ikke");
+        }
+        //Opretter en Path
+        Path musicFolderPath = saveFolder.toPath().resolve(selectedFile.getName());
+        try {
+            //Gemmer filen i mappen
+            Files.copy(selectedFile.toPath(), musicFolderPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
 }
