4e0b3ca2549f2d34ecd1a53c4f823a018198deaa
VictorBjerrumvb <144001219+VictorBjerrumvb@users.noreply.github.com>
2023-12-06 10:15:07 +0100

    Revert "loginwindowfix"
    This reverts commit 76c019b64e1368eea42e27d734cf6b798d110513.

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -2,40 +2,23 @@
 package gui;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LoginController {
-    @FXML
-    private void handleLoginButtonAction(ActionEvent event) {
-        // Perform login actions
 
-        // Get the stage (window) that contains the button
-        Stage loginStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-
-        // Close the login window
-        loginStage.close();
-
-        // Open the main window
-        openMainWindow();
-    }
-
-    private void openMainWindow() {
-        try {
+    public void Loginbtn(ActionEvent actionEvent) throws Exception {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
             Parent root = loader.load();
-            Stage mainStage = new Stage();
-            mainStage.setScene(new Scene(root));
-            mainStage.show();
-        } catch (Exception e) {
-            e.printStackTrace(); // Handle exception appropriately
+            MainControllor mainControllor = loader.getController();
+            Stage stage = new Stage();
+            stage.setTitle("Login Manager");
+            stage.setScene(new Scene(root));
+            stage.show();
         }
     }
-}
-
 
 =======
 package gui;
