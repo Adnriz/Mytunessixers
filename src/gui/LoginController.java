76c019b64e1368eea42e27d734cf6b798d110513
VictorBjerrumvb <144001219+VictorBjerrumvb@users.noreply.github.com>
2023-12-06 10:11:21 +0100

    loginwindowfix
    asd

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -1,21 +1,38 @@
 package gui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LoginController {
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        // Perform login actions
 
-    public void Loginbtn(ActionEvent actionEvent) throws Exception {
+        // Get the stage (window) that contains the button
+        Stage loginStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+        // Close the login window
+        loginStage.close();
+
+        // Open the main window
+        openMainWindow();
+    }
+
+    private void openMainWindow() {
+        try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
             Parent root = loader.load();
-            MainControllor mainControllor = loader.getController();
-            Stage stage = new Stage();
-            stage.setTitle("Login Manager");
-            stage.setScene(new Scene(root));
-            stage.show();
+            Stage mainStage = new Stage();
+            mainStage.setScene(new Scene(root));
+            mainStage.show();
+        } catch (Exception e) {
+            e.printStackTrace(); // Handle exception appropriately
         }
     }
+}
+
 
